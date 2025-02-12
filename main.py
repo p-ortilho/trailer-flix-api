3d038504b4ded790990f08122aa8677d4e4e2dff
p-ortilho <cainportilho@gmail.com>
2025-02-12 00:04:46 -0300

    feat: adicionar rota para deletar vídeos na API

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from videos_repository import get_videos
+from videos_repository import get_videos, delete_videos
 
 app = Flask(__name__)
 
@@ -24,6 +24,22 @@ def get_video():
     try:
         data = get_videos()
         return jsonify(data), 200
+    except Exception as e:
+        return jsonify(
+            {
+                'status': 'failed',
+                'message': 'Internal server error'}
+            ), 500
+
+@app.route('/videos/<int:id>', methods=['DELETE'])
+def delete_video(id):
+    try:
+        delete_videos(id)
+        return jsonify(
+            {
+                'status': 'success',
+                'message': 'Video deleted successfully'}
+            ), 200
     except Exception as e:
         return jsonify(
             {
